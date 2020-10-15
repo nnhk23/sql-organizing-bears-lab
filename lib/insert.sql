1a5f440e825ad67d53900faa4a479382e70e2ffc
Kim Nguyen <nnhk23@gmail.com>
2020-10-14 23:24:33 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "Brown", "goofy", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "M", "Black", "playful", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "White", "sassy", false);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "F", "Brown", "lazy", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "Brown", "sneaky", false);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "M", "Brown", "cranky", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "F", "Pink", "sweet", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL , 10, "M", "Brown", "aggressive", false);
